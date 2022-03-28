cca45b7a65816e1b2fe955d7cb962c6d645cfc6e
AlexFeather <alfeat@gmail.com>
2022-03-28 18:35:50 +0300

    Added countdown

--- ThreadsLessons/ThreadsLessons/MainWindow.xaml.cs
+++ ThreadsLessons/ThreadsLessons/MainWindow.xaml.cs
@@ -22,33 +22,67 @@ namespace ThreadsLessons
     /// </summary>
     public partial class MainWindow : Window
     {
+        private TimeSpan countdown = new TimeSpan(0, 0, 0);
+        private DateTime countdownEnd;
+        private readonly Object lockObject;
+        bool active = true;
+
 
 
         public MainWindow()
         {
             InitializeComponent();
             Thread fc = new Thread(FibonacciCounter);
+            Thread count = new Thread(ItsTheFinalCountdown);
+
+            lockObject = new Object();
+
             fc.IsBackground = true;
             fc.Start();
+
+            count.IsBackground = true;
+            count.Start();
+
         }
 
         private void FibonacciCounter()
         {
-            bool active = true;
+
             StringBuilder sb = new StringBuilder("0, 1");
-            int currentNumber = 1;
-            int previousNumber = 0;
+            long currentNumber = 1;
+            long previousNumber = 0;
 
             while (active)
             {
-                int temp = currentNumber;
+                long temp = currentNumber;
                 currentNumber = currentNumber + previousNumber;
                 previousNumber = temp;
                 sb.Append($", {currentNumber}");
+
                 Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(() =>
-                FibonacciTextBox.Text = sb.ToString()));
-                
-                Thread.Sleep(1000);
+                    FibonacciTextBox.Text = sb.ToString()));
+
+                lock (lockObject)
+                {
+                    countdownEnd = DateTime.Now + TimeSpan.FromMilliseconds(5000);
+                }
+                Thread.Sleep(5000);
+            }
+        }
+
+        private void ItsTheFinalCountdown()
+        {
+            while(active)
+            {
+                lock (lockObject)
+                {
+                    countdown = countdownEnd - DateTime.Now;
+                }
+
+                Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(() =>
+                    CountdownTextBox.Text = countdown.ToString()));
+
+                Thread.Sleep(100);
             }
         }
     }
