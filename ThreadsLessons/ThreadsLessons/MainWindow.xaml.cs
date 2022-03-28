f8600bb36f9b64f36c058551e91674807e7a0109
AlexFeather <alfeat@gmail.com>
2022-03-28 12:56:16 +0300

    Added Fibonacci counter

--- ThreadsLessons/ThreadsLessons/MainWindow.xaml.cs
+++ ThreadsLessons/ThreadsLessons/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace ThreadsLessons
 {
@@ -20,9 +22,34 @@ namespace ThreadsLessons
     /// </summary>
     public partial class MainWindow : Window
     {
+
+
         public MainWindow()
         {
             InitializeComponent();
+            Thread fc = new Thread(FibonacciCounter);
+            fc.IsBackground = true;
+            fc.Start();
+        }
+
+        private void FibonacciCounter()
+        {
+            bool active = true;
+            StringBuilder sb = new StringBuilder("0, 1");
+            int currentNumber = 1;
+            int previousNumber = 0;
+
+            while (active)
+            {
+                int temp = currentNumber;
+                currentNumber = currentNumber + previousNumber;
+                previousNumber = temp;
+                sb.Append($", {currentNumber}");
+                Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(() =>
+                FibonacciTextBox.Text = sb.ToString()));
+                
+                Thread.Sleep(1000);
+            }
         }
     }
 }
