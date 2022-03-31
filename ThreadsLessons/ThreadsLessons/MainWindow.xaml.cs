a1a7dec92d0093f1e16167abb7f9453daab12fb1
AlexFeather <alfeat@gmail.com>
2022-03-31 11:22:30 +0300

    Added fibonacciCounter and countdown

--- ThreadsLessons/ThreadsLessons/MainWindow.xaml.cs
+++ ThreadsLessons/ThreadsLessons/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,17 +13,71 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace ThreadsLessons
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
+        private bool fcStopper = true;
+
+        private DateTime cdUntil;
+
+        private object cdLock = new object();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            Thread fc = new Thread(FibonacciCounter);
+            fc.IsBackground = true;
+            fc.Start();
+
+            Thread cd = new Thread(ItsTheFinalCountown);
+            cd.IsBackground = true;
+            cd.Start();
+
+        }
+
+        private void FibonacciCounter()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("0, 1, ");
+
+            long currentNum = 1;
+            long previousNum = 0;
+            long temp;
+
+            while(fcStopper)
+            {
+                temp = currentNum + previousNum;
+                previousNum = currentNum;
+                currentNum = temp;
+                Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(() =>
+                    FibonacciTextBox.Text = sb.Append($"{currentNum}, ").ToString()));
+                lock (cdLock)
+                {
+                    cdUntil = DateTime.Now + TimeSpan.FromMilliseconds(5000);
+                }
+                Thread.Sleep(5000);
+            }
+        }
+
+        private void ItsTheFinalCountown() //not really
+        {
+            TimeSpan countdown;
+            while (fcStopper)
+            {
+                lock (cdLock)
+                {
+                    countdown = cdUntil - DateTime.Now;
+                }
+                Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(() =>
+                    CountdownTextBox.Text = countdown.ToString()));
+                Thread.Sleep(250);
+            }
+
         }
     }
 }
